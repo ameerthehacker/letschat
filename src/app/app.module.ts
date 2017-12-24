463ca1a9ec2cdbc0d71999698b46fe165ed9ded2
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-24 15:40:52 +0530

    Add navbar for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { environment } from "../environments/environment";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
