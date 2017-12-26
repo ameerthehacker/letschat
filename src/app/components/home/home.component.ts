1fb8d13707b385abd840b14992a78d366aaa61e2
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-26 22:16:32 +0530

    Design the home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lc-home',
   templateUrl: './home.component.html',
-  styles: []
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
